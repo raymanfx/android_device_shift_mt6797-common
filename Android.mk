6b3fc6d1ce10b4c51333a447b0d80239838d0955
Alexander Martinz <amartinz@shiftphones.com>
2018-12-13 18:35:38 +0100

    mt6797-common: create NVRAM mount points
    Change-Id: I2eb61ecf9eeb622ef510976a5758d4511bdb317d
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- Android.mk
+++ Android.mk
@@ -4,4 +4,32 @@ ifneq ($(filter SHIFT5me SHIFT6m,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
+include $(CLEAR_VARS)
+
+# Create mount points for NVRAM inside of vendor to make GSIs work
+NVCFG_MOUNT_POINT  := $(TARGET_OUT_VENDOR)/etc/nvcfg
+NVDATA_MOUNT_POINT := $(TARGET_OUT_VENDOR)/etc/nvdata
+PROTECT_F_MOUNT_POINT  := $(TARGET_OUT_VENDOR)/etc/protect_f
+PROTECT_S_MOUNT_POINT := $(TARGET_OUT_VENDOR)/etc/protect_s
+
+$(NVCFG_MOUNT_POINT):
+	@echo "Creating $(NVCFG_MOUNT_POINT)"
+	@mkdir -p $(TARGET_OUT_VENDOR)/etc/nvcfg
+
+$(NVDATA_MOUNT_POINT):
+	@echo "Creating $(NVDATA_MOUNT_POINT)"
+	@mkdir -p $(TARGET_OUT_VENDOR)/etc/nvdata
+
+$(PROTECT_F_MOUNT_POINT):
+	@echo "Creating $(PROTECT_F_MOUNT_POINT)"
+	@mkdir -p $(TARGET_OUT_VENDOR)/etc/protect_f
+
+$(PROTECT_S_MOUNT_POINT):
+	@echo "Creating $(PROTECT_S_MOUNT_POINT)"
+	@mkdir -p $(TARGET_OUT_VENDOR)/etc/protect_s
+
+ALL_DEFAULT_INSTALLED_MODULES += \
+    $(NVCFG_MOUNT_POINT) $(NVDATA_MOUNT_POINT) \
+    $(PROTECT_F_MOUNT_POINT) $(PROTECT_S_MOUNT_POINT) \
+
 endif
