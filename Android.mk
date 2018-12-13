6c7d5c12c513d6fcd968d24e49553bfc3e742b59
Alexander Martinz <amartinz@shiftphones.com>
2018-12-13 20:38:26 +0100

    mt6797-common: symlink NVRAM directories for backwards compatibility
    Change-Id: I7056bf2fa0bd02d0ab0d118eb301fc484ecdd869
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- Android.mk
+++ Android.mk
@@ -32,4 +32,29 @@ ALL_DEFAULT_INSTALLED_MODULES += \
     $(NVCFG_MOUNT_POINT) $(NVDATA_MOUNT_POINT) \
     $(PROTECT_F_MOUNT_POINT) $(PROTECT_S_MOUNT_POINT) \
 
+# Symlink old mount points for blob compatibility
+NVCFG_SYMLINK := $(TARGET_ROOT_OUT)/nvcfg
+$(NVCFG_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating NVCFG symlink: $(TARGET_ROOT_OUT)/nvcfg -> /vendor/etc/nvcfg"
+	$(hide) ln -sf /vendor/etc/nvcfg $(TARGET_ROOT_OUT)/nvcfg
+
+NVDATA_SYMLINK := $(TARGET_ROOT_OUT)/nvdata
+$(NVDATA_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating NVDATA symlink: $(TARGET_ROOT_OUT)/nvdata -> /vendor/etc/nvdata"
+	$(hide) ln -sf /vendor/etc/nvdata $(TARGET_ROOT_OUT)/nvdata
+
+PROTECT_F_SYMLINK := $(TARGET_ROOT_OUT)/protect_f
+$(PROTECT_F_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating PROTECT_F symlink: $(TARGET_ROOT_OUT)/protect_f -> /vendor/etc/protect_f"
+	$(hide) ln -sf /vendor/etc/protect_f $(TARGET_ROOT_OUT)/protect_f
+
+PROTECT_S_SYMLINK := $(TARGET_ROOT_OUT)/protect_s
+$(PROTECT_S_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating PROTECT_S symlink: $(TARGET_ROOT_OUT)/protect_s -> /vendor/etc/protect_s"
+	$(hide) ln -sf /vendor/etc/protect_s $(TARGET_ROOT_OUT)/protect_s
+
+ALL_DEFAULT_INSTALLED_MODULES += \
+    $(NVCFG_SYMLINK) $(NVDATA_SYMLINK) \
+    $(PROTECT_F_SYMLINK) $(PROTECT_S_SYMLINK) \
+
 endif
