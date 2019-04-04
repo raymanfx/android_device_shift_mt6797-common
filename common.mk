170b5798b52ff6969ad395ac239bf38061b3d998
Christopher N. Hesse <raymanfx@gmail.com>
2019-04-04 16:10:59 +0200

    mt6797-common: Add compatibility matrix
    Required so the build system is happy.
    
    Signed-off-by: Christopher N. Hesse <raymanfx@gmail.com>

--- common.mk
+++ common.mk
@@ -97,6 +97,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_hwc_vds=1 \
 
 # HIDL
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/compatibility_matrix.xml:system/compatibility_matrix.xml
+
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0
