13342c99bba3f0e3bb4dfb97591acc2397b68a2e
Christopher N. Hesse <raymanfx@gmail.com>
2019-04-04 14:12:05 +0200

    mt6797-common: Remove locale settings
    Platform should specify this, not the device.
    
    Signed-off-by: Christopher N. Hesse <raymanfx@gmail.com>

--- common.mk
+++ common.mk
@@ -1,15 +1,6 @@
 # do not restrict vendor files
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
-PRODUCT_LOCALES := \
-    en_US zh_CN zh_TW es_ES pt_BR ru_RU fr_FR de_DE tr_TR vi_VN ms_MY in_ID th_TH it_IT ar_EG hi_IN bn_IN ur_PK \
-    fa_IR pt_PT nl_NL el_GR hu_HU tl_PH ro_RO cs_CZ ko_KR km_KH iw_IL my_MM pl_PL es_US bg_BG hr_HR lv_LV lt_LT \
-    sk_SK uk_UA de_AT da_DK fi_FI nb_NO sv_SE en_GB hy_AM zh_HK et_EE ja_JP kk_KZ sr_RS sl_SI ca_ES
-
-# default to german / germany
-#PRODUCT_DEFAULT_LANGUAGE := de
-#PRODUCT_DEFAULT_REGION   := DE
-
 # overlay has priorities. high <-> low.
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
